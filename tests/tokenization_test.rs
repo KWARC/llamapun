808c9065eab9e5183b968f3c79fad13c8c88fa13
Deyan Ginev <d.ginev@jacobs-university.de>
2015-06-14 10:40:07 -0400

    precise sentence tests

--- tests/tokenization_test.rs
+++ tests/tokenization_test.rs
@@ -29,9 +29,25 @@ fn test_sentence_tokenization_simple() {
 
   let simple_tokenizer = Tokenizer::default();
   let ranges : Vec<DNMRange> = simple_tokenizer.sentences(&simple_dnm).unwrap();
-  // // Debug:
-  // for r in ranges.iter() {
-  //   println!("Sentence: \n{}\n",r.get_plaintext().trim());
-  // }
   assert_eq!(ranges.len(), 6);
+
+  let sentences_expected : Vec<&str> = vec![
+  "This note was written to clarify for myself and my colleagues certain properties of Bernstein approximations that are useful in investigating copulas.",
+  "We derive some of the basic properties of the Bernstein approximation for functions of n variables and then show that the Bernstein approximation of a copula is again a copula.",
+  "Unorthodox beginnings of sentences can also occur.",
+  "Deciphering Eqn. 1 is sometimes. difficult Prof. Automation, isn't it?",
+  "Our most significant result is a stochastic interpretation of the Bernstein approximation of a copula.",
+  "This interpretation was communicated to us by J. H. B. Kemperman in [2] for 2-copulas and we are not aware of its publication elsewhere."];
+  let r_iter = ranges.iter();
+  let mut e_iter = sentences_expected.iter();
+
+  for range in r_iter {
+    let s1 = range.get_plaintext().trim().to_owned();
+    let s2 = match e_iter.next() {
+      None => "".to_string(),
+      Some(x) => x.to_string()
+    };
+    assert_eq!(s1, s2);
+  }
+  
 }

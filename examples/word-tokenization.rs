64018e70a29a2854155a06763eec44242b5db9ce
Deyan Ginev <d.ginev@jacobs-university.de>
2015-06-24 19:38:20 -0400

    moving to a proper example; fixing plots

--- examples/word-tokenization.rs
+++ examples/word-tokenization.rs
@@ -14,7 +14,8 @@ fn main() {
 
   let doc = XmlDoc::parse_html_file("tests/resources/0903.1000.html").unwrap();
   let mut dictionary: HashMap<String, i64> = HashMap::new();
-  let mut frequencies: HashMap<String, i64> = HashMap::new();
+  let mut word_frequencies: HashMap<String, i64> = HashMap::new();
+  let mut frequencies: HashMap<i64, i64> = HashMap::new();
   let mut word_index = 0;
 
   // We will tokenize each logical paragraph, which are the textual logical units in an article
@@ -53,8 +54,9 @@ fn main() {
             &word_index }
           };
         print!("{}  ",dictionary_index);
-        let word_frequency = frequencies.entry(word).or_insert(0);
+        let word_frequency = frequencies.entry(*dictionary_index).or_insert(0);
         *word_frequency += 1;
+        word_frequencies.insert(word.clone(), word_frequency.clone());
       }
       println!("");
     }
@@ -65,46 +67,53 @@ fn main() {
   for (word, index) in dictionary.iter() {
     sorted_dictionary.push((word,index));
   }
+  sorted_dictionary.sort_by(|a,b| a.1.cmp(b.1));
   
-
-  let mut sorted_frequencies = Vec::new();
-  for (word, index) in frequencies.iter() {
-    sorted_frequencies.push((word,index));
-  }
-
   // Unsorted gnuplot of frequencies:
-  let dict_indices = sorted_dictionary.clone().into_iter().map(|entry| entry.1);
-  let freq_values = sorted_frequencies.clone().into_iter().map(|entry| entry.1);
+
+  let freq_keys = frequencies.clone().into_iter().map(|entry| entry.0);
+  let freq_values = frequencies.clone().into_iter().map(|entry| entry.1);
   let mut fg = Figure::new();
   fg.axes2d()
-  .points(dict_indices, freq_values, &[PointSymbol('D'), Color("#ffaa77"), PointSize(1.5)])
+  .points(freq_keys, freq_values, &[PointSymbol('D'), Color("#ffaa77"), PointSize(1.5)])
   .set_x_label("Words, in order of appearance", &[Rotate(45.0)])
   .set_y_label("Frequency counts", &[Rotate(90.0)])
   .set_title("Example Word Frequencies", &[]);
 
-  fg.set_terminal("pngcairo", "word_frequnecies_inorder.png");
+  fg.set_terminal("pngcairo", "word_frequencies_inorder.png");
   fg.show();
   
   // Sorted gnuplot of frequencies:
   // Perform sort
-  sorted_dictionary.sort_by(|a, b| a.1.cmp(b.1));
-  sorted_frequencies.sort_by(|a, b| a.1.cmp(b.1));
-  let sorted_dict_indices = sorted_dictionary.clone().into_iter().map(|entry| entry.1);
-  let sorted_freq_values = sorted_frequencies.clone().into_iter().map(|entry| entry.1);
+  let mut value_sorted_frequencies = Vec::new();
+  for (index,value) in frequencies.iter() {
+    value_sorted_frequencies.push((index,value));
+  }
+  value_sorted_frequencies.sort_by(|a, b| a.1.cmp(b.1));
+
+  let sorted_freq_values = value_sorted_frequencies.clone().into_iter().map(|entry| entry.1);
+  let ordered_indexes = (0..sorted_freq_values.len()).collect::<Vec<_>>().into_iter();
 
   fg = Figure::new();
   fg.axes2d()
-  .points(sorted_dict_indices, sorted_freq_values, &[PointSymbol('D'), Color("#ffaa77"), PointSize(1.5)])
+  .points(ordered_indexes, sorted_freq_values, &[PointSymbol('D'), Color("#ffaa77"), PointSize(1.5)])
   .set_x_label("Words, in ascending frequency", &[Rotate(45.0)])
   .set_y_label("Frequency counts", &[Rotate(90.0)])
   .set_title("Example Word Frequencies", &[]);
 
-  fg.set_terminal("pngcairo", "word_frequnecies_sorted.png");
+  fg.set_terminal("pngcairo", "word_frequencies_sorted.png");
   fg.show();
 
   // Print out data:
+  let mut sorted_word_frequencies =  Vec::new();
+  for (word,value) in word_frequencies.iter() {
+    sorted_word_frequencies.push((word,value));
+  }
+  sorted_word_frequencies.sort_by(|a, b| a.1.cmp(b.1));
+
   println!("Dictionary: \n{:?}\n\n", sorted_dictionary);
-  println!("Frequencies: \n{:?}\n\n", sorted_frequencies);
+  println!("V Frequencies: \n{:?}\n\n", value_sorted_frequencies);
+  println!("Frequencies: \n{:?}\n\n", sorted_word_frequencies);
 
   
 

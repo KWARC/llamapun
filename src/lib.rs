0bcade712cb7618ab3ed77c9ecb7f952c3b06457
jfschaefer <j.schaefer@jacobs-university.de>
2015-06-26 23:32:35 +0100

    integrated rust-morpha

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,14 @@
 //! The original library can be found at https://github.com/KWARC/LLaMaPUn
 
 #![feature(slice_patterns)]
+#![feature(collections)]
+
 extern crate rustlibxml;
 extern crate libc;
 extern crate regex;
 extern crate unidecode;
 extern crate gnuplot;
+extern crate rustmorpha;
 
 pub mod dnmlib;
 pub mod stopwords;
---
+++ tests/morpha_integration_test.rs
@@ -0,0 +1,10 @@
+//! Test that rust-morpha is integrated properly
+
+extern crate rustmorpha;
+
+#[test]
+fn test_that_rust_morpha_is_integrated() {
+    assert_eq!(rustmorpha::full_stem("The tilings are amazing"),
+               "the tile be amaze");
+    rustmorpha::close();
+}

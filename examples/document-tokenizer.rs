ca623e1374459439d9aff3de216e457c8e8ea292
jfschaefer <j.schaefer@jacobs-university.de>
2016-03-10 20:58:38 +0100

    added word tokenization

--- examples/document-tokenizer.rs
+++ examples/document-tokenizer.rs
@@ -8,12 +8,13 @@ use std::collections::HashMap;
 
 use libxml::xpath::Context;
 use libxml::tree::Node;
+use libxml::tree::Document as DOM;
 
 use senna::sennapath::SENNA_PATH;
 use senna::senna::{Senna, SennaParseOptions};
 
 use llamapun::data::Corpus;
-use llamapun::dnm::{DNM, DNMParameters};
+use llamapun::dnm::{DNM, DNMParameters, DNMRange};
 use llamapun::tokenizer::Tokenizer;
 
 fn get_parent_chain(from: &Node, until: &Node) -> Vec<Node> {
@@ -93,6 +94,128 @@ fn get_plaintext(node: &Node) -> (String, Vec<usize>, Vec<Node>) {
     return (plaintext, offsets, nodes);
 }
 
+fn annotate(node : Node, root: &Node, range: &DNMRange, dnm: &DNM, dom: &DOM) -> bool {
+    // find lowest parent of range
+    let (_, offsets, nodes) = get_plaintext(root); // Need to recalculate it every round
+    let start_node = &nodes[range.start];
+    let start_parents : Vec<Node> = get_parent_chain(start_node, root);
+    let end_node = &nodes[range.end-1];
+    let end_parents : Vec<Node> = get_parent_chain(end_node, root);
+
+
+    let mut si = start_parents.len() - 1;
+    let mut ei = end_parents.len() - 1;
+    while si > 0 && ei > 0 && start_parents[si-1] == end_parents[ei-1] {
+        si -= 1;
+        ei -= 1;
+    }
+
+    let common_parent = &start_parents[si];
+
+    if common_parent.is_text_node() {
+        let before = Node::new_text_node(&dom, &dnm.plaintext[range.start - offsets[range.start]..range.start]).unwrap();
+        let core = Node::new_text_node(&dom, &dnm.plaintext[range.start..range.end]).unwrap();
+        let mut textend = range.start+1;
+        while textend < offsets.len() && offsets[textend] > 0 {
+            textend += 1;
+        }
+        let after = Node::new_text_node(&dom, &dnm.plaintext[range.end..textend]).unwrap();
+
+        common_parent.add_prev_sibling(node.clone()).unwrap();
+        node.add_prev_sibling(before).unwrap();
+        let break_ = Node::new("BREAK", None, &dom).unwrap();
+        common_parent.add_prev_sibling(break_.clone()).unwrap();
+        break_.add_prev_sibling(after).unwrap();
+        break_.unlink();
+        break_.free();
+        common_parent.unlink();
+        common_parent.free();
+        node.add_child(&core).unwrap();
+    } else if common_parent == start_node && common_parent == end_node {
+        if offsets[range.start] == 0 && (range.end == offsets.len() || offsets[range.end] == 0) {
+            common_parent.add_prev_sibling(node.clone()).unwrap();
+            common_parent.unlink();
+            node.add_child(common_parent).unwrap();
+        } else {
+            writeln!(std::io::stderr(), "Warning: Couldn't split for annotation (doesn't match token boundaries): \"{}\"",
+            range.get_plaintext()).unwrap();
+        }
+    } else {
+        // make sure splitting is possible
+        let mut act_start = start_parents[si - 1].clone();
+        if !(range.start == 0 ||
+             act_start.is_text_node() ||
+             !is_child_of(&nodes[range.start-1], &act_start, &root)) {
+
+            // let act_end = &end_parents[ei - 1];
+            // writeln!(std::io::stderr(), "NAMES: \"{}\" > \"{}\" | \"{}\"", dom.node_to_string(&common_parent), dom.node_to_string(&act_start), dom.node_to_string(&act_end)).unwrap();
+            writeln!(std::io::stderr(), "Warning: Couldn't split for tokenization (at beginning): \"{}\"",
+            range.get_plaintext()).unwrap();
+            node.free();
+            return false;
+        }
+        let mut act_end = end_parents[ei - 1].clone();
+        if !(range.end == dnm.plaintext.len()-1 ||
+             act_end.is_text_node() ||
+             !is_child_of(&nodes[range.end+1], &act_end, &root)) {
+            writeln!(std::io::stderr(), "Warning: Couldn't split for tokenization (at end): \"{}\"",
+            range.get_plaintext()).unwrap();
+            node.free();
+            return false;
+        }
+
+        // split text nodes
+        if act_start.is_text_node() && offsets[range.start] != 0 {  // have to split act_start
+            let before = Node::new_text_node(&dom, &dnm.plaintext[range.start-offsets[range.start]..range.start]).unwrap();
+            let mut textend = range.start+1;
+            while offsets[textend] > 0 {  // can't run to end of array, because in that case we'd have a text node as common parent (checked for before)
+                textend += 1;
+            }
+            let after = Node::new_text_node(&dom, &dnm.plaintext[range.start..textend]).unwrap();
+            let break_ = Node::new("BREAK", None, &dom).unwrap();  // make sure text nodes don't get merged into act_start
+            act_start.add_prev_sibling(break_.clone()).unwrap();
+            break_.add_prev_sibling(before).unwrap();
+            let tmp = break_.add_prev_sibling(after).unwrap();
+            act_start.unlink();
+            act_start.free();
+            break_.unlink();
+            break_.free();
+            act_start = tmp;
+        }
+        if act_end.is_text_node() && range.end < dnm.plaintext.len() - 1 &&
+            offsets[range.end+1] != 0 {
+                let before = Node::new_text_node(&dom, &dnm.plaintext[range.end - offsets[range.end]..range.end]).unwrap();
+                let mut textend = range.end+1;
+                while textend < offsets.len() && offsets[textend] > 0 {
+                    textend += 1;
+                }
+
+                let after = Node::new_text_node(&dom, &dnm.plaintext[range.end..textend]).unwrap();
+                let stop = Node::new("STOP", None, &dom).unwrap();
+                act_end.add_prev_sibling(stop.clone()).unwrap();
+                let tmp = stop.add_prev_sibling(before).unwrap();
+
+                stop.add_prev_sibling(after).unwrap();
+                act_end.unlink();
+                act_end.free();
+                stop.unlink();
+                stop.free();
+                act_end = tmp;
+            }
+
+        act_start.add_prev_sibling(node.clone()).unwrap();
+        while act_start != act_end {    // iterate with act_start to act_end and move everything inside node
+            let tmp = act_start.get_next_sibling().unwrap();
+            act_start.unlink();
+            node.add_child(&act_start).unwrap();
+            act_start = tmp;
+        }
+        act_end.unlink();
+        node.add_child(&act_end).unwrap();
+    }
+    return true;
+}
+
 
 
 pub fn main() {
@@ -155,137 +278,29 @@ pub fn main() {
             };
             let sentences = tokenizer.sentences(&dnm);
             for sentence in sentences {
-                // find lowest parent of range
-                let (_, offsets, nodes) = get_plaintext(&para); // Need to recalculate it every round
-                let start_node = &nodes[sentence.start];
-                let start_parents : Vec<Node> = get_parent_chain(start_node, &para);
-                let end_node = &nodes[sentence.end-1];
-                let end_parents : Vec<Node> = get_parent_chain(end_node, &para);
-
-
-                let mut si = start_parents.len() - 1;
-                let mut ei = end_parents.len() - 1;
-                while si > 0 && ei > 0 && start_parents[si-1] == end_parents[ei-1] {
-                    si -= 1;
-                    ei -= 1;
-                }
-
                 let senna_parse = senna.parse(sentence.get_plaintext(), SennaParseOptions { pos: true, psg: true,});
-                
-                let common_parent = &start_parents[si];
+                let snode = Node::new("span", None, &dom).unwrap();
+                snode.add_property("class", "sentence");
+                // Colors help to easily see missing annotations
+                // snode.add_property("style", "color:red");
+                snode.add_property("id", &format!("sentence.{}", sentence_id_counter));
+                sentence_id_counter += 1;
+                snode.add_property("psg", senna_parse.get_psgstring().unwrap());
 
-                if common_parent.is_text_node() {
-                    let before = Node::new_text_node(&dom, &dnm.plaintext[sentence.start - offsets[sentence.start]..sentence.start]).unwrap();
-                    let core = Node::new_text_node(&dom, &dnm.plaintext[sentence.start..sentence.end]).unwrap();
-                    let mut textend = sentence.start+1;
-                    while textend < offsets.len() && offsets[textend] > 0 {
-                        textend += 1;
-                    }
-                    let after = Node::new_text_node(&dom, &dnm.plaintext[sentence.end..textend]).unwrap();
+                annotate(snode, &para, &sentence, &dnm, &dom);
 
-                    let snode = Node::new("span", None, &dom).unwrap();
-                    snode.add_property("class", "sentence");
-                    snode.add_property("id", &format!("sentence.{}", sentence_id_counter));
-                    sentence_id_counter += 1;
-                    snode.add_property("psg", senna_parse.get_psgstring().unwrap());
-                    common_parent.add_prev_sibling(snode.clone()).unwrap();
-                    snode.add_prev_sibling(before).unwrap();
-                    let break_ = Node::new("BREAK", None, &dom).unwrap();
-                    common_parent.add_prev_sibling(break_.clone()).unwrap();
-                    break_.add_prev_sibling(after).unwrap();
-                    break_.unlink();
-                    break_.free();
-                    common_parent.unlink();
-                    common_parent.free();
-                    snode.add_child(&core).unwrap();
-                } else if common_parent == start_node && common_parent == end_node {
-                    if offsets[sentence.start] == 0 && (sentence.end == offsets.len() || offsets[sentence.end] == 0) {
-                        let snode = Node::new("span", None, &dom).unwrap();
-                        snode.add_property("class", "sentence");
-                        snode.add_property("id", &format!("sentence.{}", sentence_id_counter));
-                        sentence_id_counter += 1;
-                        common_parent.add_prev_sibling(snode.clone()).unwrap();
-                        common_parent.unlink();
-                        snode.add_child(common_parent).unwrap();
-                    } else {
-                        writeln!(std::io::stderr(), "Warning: Couldn't split sentence (doesn't match token boundaries): \"{}\"",
-                                                    sentence.get_plaintext()).unwrap();
-                    }
-                } else {
-                    // make sure splitting is possible
-                    let mut act_start = start_parents[si - 1].clone();
-                    if !(sentence.start == 0 ||
-                           act_start.is_text_node() ||
-                            !is_child_of(&nodes[sentence.start-1], &act_start, &para)) {
+                let mut word_id_counter = 0usize;
 
-                        // let act_end = &end_parents[ei - 1];
-                        // writeln!(std::io::stderr(), "NAMES: \"{}\" > \"{}\" | \"{}\"", dom.node_to_string(&common_parent), dom.node_to_string(&act_start), dom.node_to_string(&act_end)).unwrap();
-                        writeln!(std::io::stderr(), "Warning: Couldn't split sentence (at beginning): \"{}\"",
-                                                    sentence.get_plaintext()).unwrap();
-                        continue;
-                    }
-                    let mut act_end = end_parents[ei - 1].clone();
-                    if !(sentence.end == dnm.plaintext.len()-1 ||
-                           act_end.is_text_node() ||
-                            !is_child_of(&nodes[sentence.end+1], &act_end, &para)) {
-                        writeln!(std::io::stderr(), "Warning: Couldn't split sentence (at end): \"{}\"",
-                                                    sentence.get_plaintext()).unwrap();
-                        continue;
-                    }
-                    
-                    // split text nodes
-                    if act_start.is_text_node() && offsets[sentence.start] != 0 {  // have to split act_start
-                        let before = Node::new_text_node(&dom, &dnm.plaintext[sentence.start-offsets[sentence.start]..sentence.start]).unwrap();
-                        let mut textend = sentence.start+1;
-                        while offsets[textend] > 0 {  // can't run to end of array, because in that case we'd have a text node as common parent (checked for before)
-                            textend += 1;
-                        }
-                        let after = Node::new_text_node(&dom, &dnm.plaintext[sentence.start..textend]).unwrap();
-                        let break_ = Node::new("BREAK", None, &dom).unwrap();  // make sure text nodes don't get merged into act_start
-                        act_start.add_prev_sibling(break_.clone()).unwrap();
-                        break_.add_prev_sibling(before).unwrap();
-                        let tmp = break_.add_prev_sibling(after).unwrap();
-                        act_start.unlink();
-                        act_start.free();
-                        break_.unlink();
-                        break_.free();
-                        act_start = tmp;
-                    }
-                    if act_end.is_text_node() && sentence.end < dnm.plaintext.len() - 1 &&
-                                         offsets[sentence.end+1] != 0 {
-                        let before = Node::new_text_node(&dom, &dnm.plaintext[sentence.end - offsets[sentence.end]..sentence.end]).unwrap();
-                        let mut textend = sentence.end+1;
-                        while textend < offsets.len() && offsets[textend] > 0 {
-                            textend += 1;
-                        }
-                        
-                        let after = Node::new_text_node(&dom, &dnm.plaintext[sentence.end..textend]).unwrap();
-                        let stop = Node::new("STOP", None, &dom).unwrap();
-                        act_end.add_prev_sibling(stop.clone()).unwrap();
-                        let tmp = stop.add_prev_sibling(before).unwrap();
+                for word in senna_parse.get_words() {
+                    let wnode = Node::new("span", None, &dom).unwrap();
+                    wnode.add_property("class", "word");
+                    // wnode.add_property("style", "color:blue");
+                    wnode.add_property("id", &format!("word.{}.{}", sentence_id_counter, word_id_counter));
+                    word_id_counter += 1;
+                    wnode.add_property("pos", word.get_pos().to_str());
 
-                        stop.add_prev_sibling(after).unwrap();
-                        act_end.unlink();
-                        act_end.free();
-                        stop.unlink();
-                        stop.free();
-                        act_end = tmp;
-                    }
-
-                    let snode = Node::new("span", None, &dom).unwrap();
-                    snode.add_property("class", "sentence");
-                    snode.add_property("id", &format!("sentence.{}", sentence_id_counter));
-                    sentence_id_counter += 1;
-                    snode.add_property("psg", senna_parse.get_psgstring().unwrap());
-                    act_start.add_prev_sibling(snode.clone()).unwrap();
-                    while act_start != act_end {    // iterate with act_start to act_end and move everything inside snode
-                        let tmp = act_start.get_next_sibling().unwrap();
-                        act_start.unlink();
-                        snode.add_child(&act_start).unwrap();
-                        act_start = tmp;
-                    }
-                    act_end.unlink();
-                    snode.add_child(&act_end).unwrap();
+                    let word_range = sentence.get_subrange(word.get_offset_start(), word.get_offset_end());
+                    annotate(wnode, &para, &word_range, &dnm, &dom);
                 }
             }
         }

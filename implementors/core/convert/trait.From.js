aff62c46bb65a012b6b1b797e919733b0e92d715
dginev <dginev@users.noreply.github.com>
2022-11-17 00:16:36 +0000

    Automated deployment: Thu Nov 17 00:16:36 UTC 2022 9f4365d612f9295795612e40f1ed3516d07df67c

--- implementors/core/convert/trait.From.js
+++ implementors/core/convert/trait.From.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["llamapun"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.63.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.63.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"llamapun/ams/enum.StructuralEnv.html\" title=\"enum llamapun::ams::StructuralEnv\">StructuralEnv</a>","synthetic":false,"types":["llamapun::ams::StructuralEnv"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"llamapun":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.65.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"llamapun/ams/enum.StructuralEnv.html\" title=\"enum llamapun::ams::StructuralEnv\">StructuralEnv</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/llamapun/parallel_data/trait.XPathFilteredIterator.js
+++ implementors/llamapun/parallel_data/trait.XPathFilteredIterator.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["llamapun"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"llamapun":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

4b6c62dbf459a3208f0c2a8219cb83e92410f186
dginev <dginev@users.noreply.github.com>
2023-01-11 23:06:43 +0000

    Automated deployment: Wed Jan 11 23:06:43 UTC 2023 283be4ac01e71e10400f7117b6abef127b130c11

--- implementors/core/convert/trait.From.js
+++ implementors/core/convert/trait.From.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"llamapun":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.65.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"llamapun/ams/enum.StructuralEnv.html\" title=\"enum llamapun::ams::StructuralEnv\">StructuralEnv</a>"]]
+"llamapun":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.66.1/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"llamapun/ams/enum.StructuralEnv.html\" title=\"enum llamapun::ams::StructuralEnv\">StructuralEnv</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
